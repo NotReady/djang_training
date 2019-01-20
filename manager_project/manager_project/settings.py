0e70d84b313d95d89ba1f6ef1f4b4378ff99d9b4
notready <loreley.lala@gmail.com>
2019-01-20 16:40:00 +0900

    テンプレートの実装

--- manager_project/manager_project/settings.py
+++ manager_project/manager_project/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 追加のasset
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)

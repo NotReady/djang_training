f250d3b625b9850cb34fa4786f3d37c5762eb079
notready <loreley.lala@gmail.com>
2019-01-20 16:38:35 +0900

    ・モデルの属性スペル修正 ・やめた時期にnullオプションを追加

--- manager_project/manager/models.py
+++ manager_project/manager/models.py
@@ -75,10 +75,10 @@ class Manager(models.Model):
 
 class Worker(models.Model):
     # 人
-    pelson = models.ForeignKey('Person', on_delete=models.PROTECT)
+    person = models.ForeignKey('Person', on_delete=models.PROTECT)
     # 着任時期
     joined_at = models.DateTimeField()
     # やめた時期
-    quited_at = models.DateTimeField()
+    quited_at = models.DateTimeField(null=True, blank=True)
     # 担当上司
     manager = models.ForeignKey('Manager', on_delete=models.PROTECT)

887fdd6eba512a0b295be169ae7575a2aa37edb9
notready <loreley.lala@gmail.com>
2019-01-20 16:39:30 +0900

    アプリのルーティングとビューの実装

--- manager_project/manager_project/urls.py
+++ manager_project/manager_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from manager import views as manager_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('worker_list/', manager_view.WorkerListView.as_view())  # URLとViewを組み合わせる
 ]
